46977843a43221af737d472d10d9e361f19b38af
NonsoEz <itsnonso97@yahoo.com>
2020-01-22 12:28:28 +0100

    First three requirements are working

--- CLI_BankingApp.py
+++ CLI_BankingApp.py
@@ -5,12 +5,12 @@ class BankApp:
     def __init__(self):
         self.users=[]
 
-        #self.prompt()
+        self.prompt()
             
-        with open('usersdata.json') as json_file :
+        with open('usersdata.json') as json_file:
             self.users = json.load(json_file)
 
-    """def prompt(self):
+    def prompt(self):
         print("Welcome to Virtual Gardens Group Bank")
         print("Press 1: Create Account\nPress 2: Transaction\nPress 3: To Quit\n")
         try:
@@ -26,20 +26,22 @@ class BankApp:
             quit()
         else:
             print("\nPlease choose 1, 2 or 3\n")
-            self.prompt()"""
+            self.prompt()
 
 
     def create_user(self):
+        #opens file for reading
+        with open('usersdata.json', 'r') as json_file:
+            data = json.load(json_file)
         email = input("Enter email: ").lower()
         if ("@" in email) and ("." in email):
-            #check if user exists
-            if email in self.users:
-                print("User already exists ")
-                return self.create_user()
+            #check if email exists in json file
+            if email in ([sub['email'] for sub in data]):
+                print("Email already exists, please use a unique email ")
+                self.create_user()
             else:
                 try:
-                    """ 
-                    pin variable is retrieved as a string object to enable the len() function to 
+                    """pin variable is retrieved as a string object to enable the len() function to 
                     check number of characters. Function int(pin) enables ValueError to check for
                     values that are not integers but does not change the datatype of pin.
                     """
@@ -62,116 +64,56 @@ class BankApp:
                 bal = user["balance"]
                 print(f"Account created successfully! Your account balance is: {bal}")
                 print(self.users)
-                #self.prompt()
+                self.prompt()
         else:
             print("Email is not valid, Please try again")
             self.create_user()
 
         
     def transaction(self):
-        pass
-        '''with open('usersdata.json') as json_file :
+        #opens file for reading
+        with open('usersdata.json') as json_file :
             data = json.load(json_file)
         print("Please login")
         # ask the user to enter email
-        email = input("Enter email: ")
-        if email not in data.keys():
-            print("User does not exist.\nKindly re-enter correct details or create an account")
-            self.prompt()
-        else:
+        login_email = input("Enter email: ")
+        # check if email exists in json file
+        if login_email in ([sub['email'] for sub in data]):
             # check if pin is a match
-            pin = int(input("Enter pin: "))
-            if pin in data.values():
-
-        
-        if login_pin not in data:
-            print("Invalid pin")
-            return self.transaction()
-        else:
-            transaction_prompt = (input("Press 1: Check Balance \nPress 2: Deposit \nPress 3: Withdraw \nPress 4: Transfer \n>> " ))
-            # display actions to perform
-            if transaction_prompt == 1:
-                return self.check_balance()
-            elif transaction_prompt == 2:
-                return self.deposit()
-            elif transaction_prompt == 3:
-                return self.withdraw()
-            elif transaction_prompt == 4:
-                return self.transfer()
+            #might want to put an exception handler here
+            login_pin = int(input("Enter pin: "))
+            if login_pin in ([sub['pin'] for sub in data]):
+                print("Login successful!\nPlease proceed to select transaction")
+                transaction_prompt = (input("Press 1: Check Balance \nPress 2: Deposit \nPress 3: Withdraw \nPress 4: Transfer \n>> " ))
+                # display actions to perform
+                if transaction_prompt == 1:
+                    return self.check_balance(login_email)
+                """elif transaction_prompt == 2:
+                    return self.deposit(login_email, login_pin)
+                elif transaction_prompt == 3:
+                    return self.withdraw(login_email, login_pin)
+                elif transaction_prompt == 4:
+                    return self.transfer(login_email, login_pin)"""
             else:
-                print("Incorrect entry")
+                print("Invalid pin")
                 self.transaction()
-        '''
-
-#creating an object of class
-s = BankApp()
-    
-#calling functions with that class object
-"""s.prompt()"""
-s.create_user()
-
-
-
-
-"""def transaction(self):
-        # ask the user to enter pin
-        #email = input("Enter email: ")
-        login_prompt = int(input("Enter 4-digit pin to proceed: "))
-        # check if pin is a match
-        with open("usersdata.json") as userdetail:
-            data = json.load(userdetail)
-        if login_prompt not in data:
-            print("Invalid pin")
-            return self.transaction()
         else:
-            transaction_prompt = (input("Press 1: Check Balance \nPress 2: Deposit \nPress 3: Withdraw \nPress 4: Transfer \n>> " )):
-            # display actions to perform
-            if transaction_prompt == 1:
-                return self.check_balance()
-            elif transaction_prompt == 2:
-                return self.deposit()
-            elif transaction_prompt == 3:
-                return self.withdraw()
-            elif transaction_prompt == 4:
-                return self.transfer()
-            else:
-                print("Incorrect entry")
-                self.transaction()
-    
-    def transaction(self): 
-        #ask the user to enter pin
-        email = input("Enter email: ")
-        login_prompt = int(input("Enter 4-digit pin to proceed: "))
-        #check if pin is a match
-        with open("usersdata.json") as userdetail:
-            data = json.load(userdetail)
-        if login_prompt not in data:
-            print("Invalid pin")
-            return self.transaction()
-        elif transaction_prompt = (int(input("Press 1: Check Balance \nPress 2: Deposit \nPress 3: Withdraw \nPress 4: Transfer \n>> " ))):
-            #display actions to perform
-            if transaction_prompt == 1:
-                return check_balance()
-            elif transaction_prompt == 2:
-                return deposit()
-            elif transaction_prompt == 3:
-                return withdraw()
-            elif transaction_prompt == 4:
-                return transfer()
-            else:
-                print("Incorrect entry")
-                return transaction()
+            print("Invalid email")
+            self.prompt()
 
-    def check_balance(self):
-        print(f"Your balance is: N{self.account_balance}")
+
+    def check_balance(self, login_email):
+        #opens json file for reading
+        with open('usersdata.json') as json_file :
+            data = json.load(json_file)
+        print(f"Your balance is: N{self.balance}")
         return self.transaction()
 
 
-    def deposit(self):
-        global account_balance
+    """def deposit(self, login_email, login_pin):
         deposit_amount = float(input("Enter amount to deposit: "))
-        account_balance += deposit_amount
-        print(f"Transaction successful! \nYour new account balance is N{account_balance}")
+        balance += deposit_amount
+        print(f"Transaction successful! \nYour new account balance is N{balance}")
         print("Thanks for banking with us!")
         return self.transaction()
 
@@ -204,3 +146,9 @@ s.create_user()
 
 
 
+#creating an object of class
+s = BankApp()
+    
+#calling functions with that class object
+"""s.prompt()
+s.create_user()"""
